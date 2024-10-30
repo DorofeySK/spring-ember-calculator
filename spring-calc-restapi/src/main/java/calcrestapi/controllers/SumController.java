cb84322c313d0390d5c34fc85187d6cb7e70dbde
Dorofey <dorofeiskripnikov@yandex.ru>
2024-10-30 20:15:51 +0500

    Controllers for base math operations

--- spring-calc-restapi/src/main/java/calcrestapi/controllers/SumController.java
+++ spring-calc-restapi/src/main/java/calcrestapi/controllers/SumController.java
@@ -1,21 +1,25 @@
 package calcrestapi.controllers;
 
-import java.math.BigDecimal;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import calcrestapi.message.Request;
+import calcrestapi.message.Response;
 import calcrestapi.opers.IMathOper;
-import calcrestapi.rq.Request;
 
 @RestController
 @RequestMapping(path = "/api/sum", produces = "application/json")
 public class SumController {
 	@PostMapping(consumes = "application/json")
-	public BigDecimal getSum(@RequestBody Request rq) {
+	public Map<String, Object> getSum(@RequestBody Request rq) {
 		IMathOper sum = (x, y) -> x.add(y); //{return x.add(y);}; - work too
-		return sum.result(rq.getFirst(), rq.getSecond());
+		return new Response().add(
+			"sum", 
+			sum.result(rq.getFirst(), rq.getSecond())
+		).getRs();
 	}
 }
