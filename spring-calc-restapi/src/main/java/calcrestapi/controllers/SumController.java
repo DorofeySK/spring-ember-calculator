17a61cd853568138f524ba51a0a1584197a9700f
DorofeySK <dorofeiskripnikov@yandex.ru>
2024-11-11 20:48:37 +0500

    CrossOrigin annotation

--- spring-calc-restapi/src/main/java/calcrestapi/controllers/SumController.java
+++ spring-calc-restapi/src/main/java/calcrestapi/controllers/SumController.java
@@ -2,6 +2,7 @@ package calcrestapi.controllers;
 
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +14,10 @@ import calcrestapi.opers.IMathOper;
 
 @RestController
 @RequestMapping(path = "/api/sum", produces = "application/json")
+
 public class SumController {
 	@PostMapping(consumes = "application/json")
+	@CrossOrigin
 	public Map<String, Object> getSum(@RequestBody Request rq) {
 		IMathOper sum = (x, y) -> x.add(y); //{return x.add(y);}; - work too
 		return new Response().add(
